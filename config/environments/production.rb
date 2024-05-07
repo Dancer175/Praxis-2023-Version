157235d261fd2bc4301ddf5c3ccf14bb65cf8b04
Daniela Zimmermann <ceridween@protonmail.com>
2024-05-07 20:44:21 +0200

    prepared for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,11 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  # list of configured domain names
+  config.hosts << 'praxisfuersmenschsein.ch'
+  config.hosts << 'www.praxisfuersmenschsein.ch'
+  config.hosts << 'xn--praxisfrsmenschsein-cbc.ch'
+  config.hosts << 'www.xn--praxisfrsmenschsein-cbc.ch'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -18,7 +22,8 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  # per default commented, i decommented it
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -28,7 +33,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
