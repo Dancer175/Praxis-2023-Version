c846341fa470ae6acf32b18b88abd39eef847e73
Daniela Zimmermann <ceridween@protonmail.com>
2024-05-07 12:08:04 +0200

    fixed image to text button for mobile version

--- app/javascript/components/init_visibility_switch.js
+++ app/javascript/components/init_visibility_switch.js
@@ -3,6 +3,8 @@ const initVisibilitySwitch = () => {
 const visibilityButton = document.querySelector('#visibility-switch');
 const textField = document.querySelector('.page-text');
 const backgroundBox = document.querySelector('.nav-folders-wrapper');
+const showImage = document.querySelector('#show-image');
+const showText = document.querySelector('#show-text');
 
 textField.style.display = 'block';
 backgroundBox.style.display = 'block';
@@ -11,15 +13,15 @@ visibilityButton.addEventListener('click', function() {
   if (textField.style.display === "none") {
     backgroundBox.style.display = 'block';
     textField.style.display = "block";
+    showImage.style.display = "block";
+    showText.style.display = "none";
   } else {
     backgroundBox.style.display = 'none';
     textField.style.display = "none";
+    showImage.style.display = "none";
+    showText.style.display = "block";
   }
   });
-
-  // infoCross.addEventListener('click', function() {
-  //   infoBar.style.display = "none";
-  // });
 }
 
 export { initVisibilitySwitch };
